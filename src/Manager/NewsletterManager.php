1d2382ea1a0d72cbe386551f3749a38f6b2fc43b
Guilfred <guilfred.assezo@gmail.com>
2022-03-06 11:15:17 +0100

    fix(Newsletter): Fix Validation system and footer info

--- src/Manager/NewsletterManager.php
+++ src/Manager/NewsletterManager.php
@@ -12,13 +12,7 @@ class NewsletterManager extends Manager
 {
     public function subscription(ValidatorInterface $validator, string $email, string $addressIp)
     {
-        $suscriber = new Suscriber($addressIp);
-        $suscriber
-            ->setEmail($email)
-            ->setConfirmToken(GenerateToken::getGenerateConfirmationToken())
-        ;
-
-        if ($validator->validate($suscriber)->count() > 0) {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             return new JsonResponse(
                 ['message' => 'Erreur lors de l\'inscription', 'status' => 'fail'],
                 Response::HTTP_BAD_REQUEST,
@@ -26,17 +20,7 @@ class NewsletterManager extends Manager
             );
         }
 
-        $this->em->persist($suscriber);
-        $this->em->flush();
-
-        $this->email->createEmail(
-            $suscriber->getEmail(),
-            'Confirmation d\'inscription Bandama Market Newsletter',
-            'Email/susbcribe_email.html.twig', [
-                'localUrl' => $this->localUrl,
-                'url' => sprintf('%s/confirm/newsletter/%s', $this->localUrl, $suscriber->getConfirmToken())
-            ]
-        );
+        $this->validatorSubscriber($validator, $email,$addressIp);
 
         return new JsonResponse(
             ['message' => 'Votre inscription a bien été effectuée.', 'status' => 'success'],
@@ -52,5 +36,27 @@ class NewsletterManager extends Manager
             ->setEnable(true)
             ->setConfirmToken(GenerateToken::getGenerateConfirmationToken())
         ;
+    }
+
+    private function validatorSubscriber(ValidatorInterface $validator, string $email, string $addressIp)
+    {
+        $suscriber = new Suscriber($addressIp);
+        $suscriber
+            ->setEmail($email)
+            ->setConfirmToken(GenerateToken::getGenerateConfirmationToken())
+        ;
+        if ($validator->validate($suscriber)->count() === 0) {
+            $this->em->persist($suscriber);
+            $this->em->flush();
+
+            $this->email->createEmail(
+                $suscriber->getEmail(),
+                'Confirmation d\'inscription Bandama Market Newsletter',
+                'Email/susbcribe_email.html.twig', [
+                    'localUrl' => $this->localUrl,
+                    'url' => sprintf('%s/confirm/newsletter/%s', $this->localUrl, $suscriber->getConfirmToken())
+                ]
+            );
+        }
     }
 }
